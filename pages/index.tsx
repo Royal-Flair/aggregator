566b818d1f0b825b0e6d2aebb77f63f55e8a4e7a
Liam Arbuckle <liam@skinetics.tech>
2024-01-15 17:31:04 +1100

    🎯🏢 ↝ downgrading commands for capacitor integration

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,6 @@
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import Layout from '@/components/Core/Layout'
 import BaseUserFeed from '@/components/Feed';
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (      
